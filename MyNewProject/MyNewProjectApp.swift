3ee5a458a538572941ea9efc77422fe8f0df7e7b
eukelios <dbx_u@icloud.com>
2021-04-23 14:09:52 +0200

    Initial Commit

---
+++ MyNewProject/MyNewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNewProjectApp.swift
+//  MyNewProject
+//
+//  Created by Sven Nueesch on 23.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct MyNewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
